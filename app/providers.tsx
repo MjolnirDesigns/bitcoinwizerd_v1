06e1b3155d41a6253c1d21a5ca2319202eba0ae2
AlphaTrader84 <cdc0007@gmail.com>
2025-07-28 22:08:32 -0400

    Add Providers component for client-side SessionProvider

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,10 +3,6 @@
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 
-interface ProvidersProps {
-  children: ReactNode;
-}
-
-export function Providers({ children }: ProvidersProps) {
+export function Providers({ children }: { children: ReactNode }) {
   return <SessionProvider>{children}</SessionProvider>;
 }
