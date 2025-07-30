a77ff85493a9bd0f66f8f0a06ba95436a5ddaf11
AlphaTrader84 <cdc0007@gmail.com>
2025-07-30 18:53:49 -0400

    Update eslint.config.mjs to disable no-require-imports rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off', // Disable the rule to allow require()
+    },
+  },
 ];
 
 export default eslintConfig;
