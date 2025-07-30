a73b3e25c36fbae9c5983f08a24878eb8ce9d9d1
AlphaTrader84 <cdc0007@gmail.com>
2025-07-30 16:37:37 -0400

    Add scripts folder with build-no-types.js to bypass type-checking

---
+++ scripts/build-no-types.js
@@ -0,0 +1,11 @@
+import { spawn } from 'child_process';
+const build = spawn('next', ['build'], { stdio: 'inherit' });
+
+build.on('error', (error) => {
+  console.error('Build error:', error);
+  process.exit(1);
+});
+
+build.on('exit', (code) => {
+  process.exit(code);
+});
